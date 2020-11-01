a6258531b3e0ffbfddfc61c6c3498286a185022c
Jorge Sepulveda <jrg.sepulveda@gmail.com>
2020-11-01 11:31:13 -0500

    update files and code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,7 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+
 const FILES_TO_CACHE = [
     '/index.html',
     '/favicon.ico',
@@ -5,11 +9,9 @@ const FILES_TO_CACHE = [
     '/index.js',
     '/style.css',
     '/db.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
 ];
-
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
 
 self.addEventListener("install", function (evt) {
     evt.waitUntil(

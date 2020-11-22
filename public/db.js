7c9fc2329fa0b6c58d7b6537b135be3b59d8e052
Jorge Sepulveda <jrg.sepulveda@gmail.com>
2020-11-22 14:11:14 -0500

    fixed error with heroku

--- public/db.js
+++ public/db.js
@@ -1,58 +1,82 @@
-let db;
-const request = indexedDB.open("budget", 1);
+"use strict";
 
-request.onupgradeneeded = ({ target }) => {
-  let db = target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
+const pendingObjectStoreName = `pending`;
+
+// create a new db request for a "budget" database.
+const request = indexedDB.open(`budget`, 2);
+
+request.onupgradeneeded = event => {
+    const db = request.result;
+
+    // create object store called "pending" and set autoIncrement to true
+    // const db = event.target.result;
+    console.log(event);
+
+    if (!db.objectStoreNames.contains(pendingObjectStoreName)) {
+        db.createObjectStore(pendingObjectStoreName, { autoIncrement: true });
+    }
 };
 
-request.onsuccess = ({ target }) => {
-  db = target.result;
-
-  // check if app is online before reading from db
-  if (navigator.onLine) {
-    checkDatabase();
-  }
+request.onsuccess = event => {
+    console.log(`Success! ${event.type}`);
+    // check if app is online before reading from db
+    if (navigator.onLine) {
+        checkDatabase();
+    }
 };
 
-request.onerror = function(event) {
-  console.log("Woops! " + event.target.errorCode);
-};
-
-function saveRecord(record) {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
-
-  store.add(record);
-}
+request.onerror = event => console.error(event);
 
 function checkDatabase() {
-  const transaction = db.transaction(["pending"], "readwrite");
-  const store = transaction.objectStore("pending");
-  const getAll = store.getAll();
+    const db = request.result;
 
-  getAll.onsuccess = function() {
-    if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
+    // open a transaction on your pending db
+    let transaction = db.transaction([pendingObjectStoreName], `readwrite`);
+
+    // access your pending object store
+    let store = transaction.objectStore(pendingObjectStoreName);
+
+    // get all records from store and set to a variable
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch(`/api/transaction/bulk`, {
+                method: `POST`,
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: `application/json, text/plain, */*`,
+                    "Content-Type": `application/json`
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+                    // if successful, open a transaction on your pending db
+                    transaction = db.transaction([pendingObjectStoreName], `readwrite`);
+
+                    // access your pending object store
+                    store = transaction.objectStore(pendingObjectStoreName);
+
+                    // clear all items in your store
+                    store.clear();
+                });
         }
-      })
-      .then(response => {        
-        return response.json();
-      })
-      .then(() => {
-        // delete records if successful
-        const transaction = db.transaction(["pending"], "readwrite");
-        const store = transaction.objectStore("pending");
-        store.clear();
-      });
-    }
-  };
+    };
+}
+
+// eslint-disable-next-line no-unused-vars
+function saveRecord(record) {
+    const db = request.result;
+
+    // create a transaction on the pending db with readwrite access
+    const transaction = db.transaction([pendingObjectStoreName], `readwrite`);
+
+    // access your pending object store
+    const store = transaction.objectStore(pendingObjectStoreName);
+
+    // add record to your store with add method.
+    store.add(record);
 }
 
 // listen for app coming back online
-window.addEventListener("online", checkDatabase);
+window.addEventListener(`online`, checkDatabase);
